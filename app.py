834d33d0140aaf6a0ad08d0991cc17b4217ea093
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-22 16:52:44 -0400

    Complex Query 1
    This query shows all the bookings of the user

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 from pymongo import MongoClient
+import secrets
 
 app = Flask(__name__, template_folder='templates')
+app.secret_key = secrets.token_hex(16)
 
 # Connect to the MongoDB database
 client = MongoClient('mongodb://localhost:27017/')
@@ -23,15 +25,11 @@ def login():
 
     if user:
         user_name = user["userDetails"]["userName"]
-        print(user_name)
-        print(email)
         booking = bookings.find_one({"userEmail" : email})
         if booking:
-            all_flights = booking["userFlights"]
-            print(all_flights)
-        user_flight = "tbd"
-        airline_name = "tbd"
-        return redirect(url_for("homepage", user_name = user_name, user_flight = user_flight, airline_name = airline_name))
+            user_flights = booking["userFlights"]
+            session["user_flights"] = user_flights
+        return redirect(url_for("homepage", user_name = user_name))
         
     else:
         return 'Invalid credentials'
@@ -39,10 +37,16 @@ def login():
 @app.route("/homepage")
 def homepage():
     user_name = request.args.get("user_name")
-    user_flight = request.args.get("user_flight")
-    airline_name = request.args.get("airline_name")
-    user_bookings = request.args.get("user_bookings")
-    return render_template("homepage.html", user_name = user_name, user_flight = user_flight, airline_name = airline_name)
+    user_flights = session.get("user_flights")
+
+    user_flights_dict = {}
+
+    for flight in user_flights:
+        find_flight = flights.find_one({"flight_id" : flight})
+        user_flights_dict[find_flight["flight_id"]] = [find_flight["flight_details"]["airlineName"], find_flight["flight_details"]["fromLocation"], find_flight["flight_details"]["fromLocationAirportCode"],find_flight["flight_details"]["toLocation"], find_flight["flight_details"]["toLocationAirportCode"],find_flight["flight_details"]["departureTime"], find_flight["flight_details"]["arrivalTime"], find_flight["flight_details"]["duration"], find_flight["flight_details"]["totalSeats"] ]
+
+    print(user_flights_dict)
+    return render_template("homepage.html", user_name = user_name, user_flights_dict = user_flights_dict)
 
 if __name__ == '__main__':
     app.run(debug=True)
