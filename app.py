e736e314b9233f2d65fec280c8ed3fbe3b3a0e8c
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-22 17:57:33 -0400

    Format time and centered bookings

--- app.py
+++ app.py
@@ -41,9 +41,13 @@ def homepage():
 
     user_flights_dict = {}
 
+    def split_time(time_):
+        time_ = time_.split("T")
+        return time_[0] + " " + time_[1][0:-1]
+
     for flight in user_flights:
         find_flight = flights.find_one({"flight_id" : flight})
-        user_flights_dict[find_flight["flight_id"]] = [find_flight["flight_details"]["airlineName"], find_flight["flight_details"]["fromLocation"], find_flight["flight_details"]["fromLocationAirportCode"],find_flight["flight_details"]["toLocation"], find_flight["flight_details"]["toLocationAirportCode"],find_flight["flight_details"]["departureTime"], find_flight["flight_details"]["arrivalTime"], find_flight["flight_details"]["duration"], find_flight["flight_details"]["totalSeats"] ]
+        user_flights_dict[find_flight["flight_id"]] = [find_flight["flight_details"]["airlineName"], find_flight["flight_details"]["fromLocation"], find_flight["flight_details"]["fromLocationAirportCode"],find_flight["flight_details"]["toLocation"], find_flight["flight_details"]["toLocationAirportCode"],split_time(find_flight["flight_details"]["departureTime"]), split_time(find_flight["flight_details"]["arrivalTime"]), find_flight["flight_details"]["duration"], find_flight["flight_details"]["totalSeats"] ]
     return render_template("homepage.html", user_name = user_name, user_flights_dict = user_flights_dict)
 
 if __name__ == '__main__':
