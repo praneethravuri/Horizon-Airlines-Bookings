accbc5a871817afa9e6202b215afe0119dc40891
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-22 01:09:43 -0400

    Created login.html and homepage.html

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 
 app = Flask(__name__, template_folder='templates')
@@ -7,6 +7,7 @@ app = Flask(__name__, template_folder='templates')
 client = MongoClient('mongodb://localhost:27017/')
 db = client['airportDB']
 users = db['users']
+flights = db["flights"]
 
 @app.route('/')
 def index():
@@ -20,9 +21,21 @@ def login():
     user = users.find_one({'user_email': email, 'user_details.user_password': password})
 
     if user:
-        return 'Login successful!'
+        user_name = user["user_details"]["user_name"]
+        user_flight = user["user_details"]["user_flight"]
+        current_flight = flights.find_one({"flight_id" : user_flight})
+        if current_flight :
+            airline_name = current_flight["flight_details"]["airlineName"]
+        return redirect(url_for("homepage", user_name = user_name, user_flight = user_flight, airline_name = airline_name))
     else:
         return 'Invalid credentials'
+    
+@app.route("/homepage")
+def homepage():
+    user_name = request.args.get("user_name")
+    user_flight = request.args.get("user_flight")
+    airline_name = request.args.get("airline_name")
+    return render_template("homepage.html", user_name = user_name, user_flight = user_flight, airline_name = airline_name)
 
 if __name__ == '__main__':
     app.run(debug=True)
