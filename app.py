f4fed8db59fca4b8f24f6d5d94703c744c17adb0
Ravuri Praneeth <ravuripraneeth2500@gmail.com>
2023-04-22 21:30:55 -0400

    No bookings message

--- app.py
+++ app.py
@@ -28,8 +28,11 @@ def login():
         booking = bookings.find_one({"userEmail" : email})
         if booking:
             user_flights = booking["userFlights"]
+            
             session["user_flights"] = user_flights
-        return redirect(url_for("homepage", user_name = user_name))
+            return redirect(url_for("homepage", user_name = user_name))
+        else:
+            return render_template("homepage.html", error = "No Bookings")
         
     else:
         return render_template("login.html", error = "Invalid Credentials")
@@ -38,19 +41,22 @@ def login():
 def homepage():
     user_name = request.args.get("user_name")
     user_flights = session.get("user_flights")
+    if user_flights == None:
+        user_flights_dict = {}
 
-    user_flights_dict = {}
+    else:
+        user_flights_dict = {}
+        def split_time(time_):
+            time_ = time_.split("T")
+            return time_[0] + " " + time_[1][0:-1]
 
-    def split_time(time_):
-        time_ = time_.split("T")
-        return time_[0] + " " + time_[1][0:-1]
+        for flight in user_flights:
+            find_flight = flights.find_one({"flight_id" : flight})
+            user_flights_dict[find_flight["flight_id"]] = [find_flight["flight_details"]["airlineName"], find_flight["flight_details"]["fromLocation"], find_flight["flight_details"]["fromLocationAirportCode"],find_flight["flight_details"]["toLocation"], find_flight["flight_details"]["toLocationAirportCode"],split_time(find_flight["flight_details"]["departureTime"]), split_time(find_flight["flight_details"]["arrivalTime"]), find_flight["flight_details"]["duration"], find_flight["flight_details"]["totalSeats"] ]
 
-    for flight in user_flights:
-        find_flight = flights.find_one({"flight_id" : flight})
-        user_flights_dict[find_flight["flight_id"]] = [find_flight["flight_details"]["airlineName"], find_flight["flight_details"]["fromLocation"], find_flight["flight_details"]["fromLocationAirportCode"],find_flight["flight_details"]["toLocation"], find_flight["flight_details"]["toLocationAirportCode"],split_time(find_flight["flight_details"]["departureTime"]), split_time(find_flight["flight_details"]["arrivalTime"]), find_flight["flight_details"]["duration"], find_flight["flight_details"]["totalSeats"] ]
+    has_bookings = bool(user_flights_dict)
 
-    print(user_flights_dict)
-    return render_template("homepage.html", user_name = user_name, user_flights_dict = user_flights_dict)
+    return render_template("homepage.html", user_name = user_name, user_flights_dict = user_flights_dict, has_bookings = has_bookings)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/JS/invalidCredentials.js
+++ static/JS/invalidCredentials.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function(){
     $(".error-box").show();
     $(".error-box-content").text("Invalid Credentials. Please enter again");
